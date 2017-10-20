0d75fcdc69b763d00a44896f69486dfbb1c7d028
Michael <mkallgren08@gmail.com>
2017-10-20 16:14:25 -0400

    Initial Upload

---
+++ server.js
@@ -0,0 +1,55 @@
+// ====================================
+//      Dependencies
+// ====================================
+
+const bodyParser = require("body-parser")
+const logger = require("morgan")
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+// Required Models
+
+// Initialize Express
+const app = express();
+// Set the port to use as a variable.
+const port = process.env.PORT || 3000;
+
+
+// Sets up the main handlebars page (main.hbs) to serve our web apps pages
+// Sets the viewing engine of the app to handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+//Use body-parser and morgan with the app
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(logger("dev"));
+
+// set the app to listen for a server connection
+app.listen(port, function () {
+    console.log('App listening on port ' + port)
+})
+
+// ====================================
+//      Routing
+// ====================================
+
+// Homepage Route
+app.get('/', function (req, res) {
+    let hbsObject = {
+        title: "Homepage - Michael Kallgren",
+        mainActive: 'active',
+        results: res
+    }
+    // console.log("hbsObj for rendering: " + JSON.stringify(hbsObject), null, 2);
+    res.render("homepage.handlebars", hbsObject);
+});
